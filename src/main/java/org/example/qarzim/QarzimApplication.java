aeb0d4b78fb80a39f3123c7bd4bc6f1128fe2700
ElyorAzimovJava <ayubjonyahiyoyev@gmail.com>
2024-08-26 19:10:15 +0500

    :Project started

---
+++ src/main/java/org/example/qarzim/QarzimApplication.java
@@ -0,0 +1,13 @@
+package org.example.qarzim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QarzimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QarzimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/qarzim/QarzimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qarzim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QarzimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
